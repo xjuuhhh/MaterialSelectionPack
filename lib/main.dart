e35399a6eaa5380c227863ae0e972b866bec56ed
xjuuhhh <juliasantoscursos05@gmail.com>
2026-09-16 11:11:45 -0300

    slide 10

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MainApp extends StatelessWidget {
       title: "Agendamento de Evento",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 255, 0, 0)),
         useMaterial3: true,
       ),
       // Aponta Home para Classe AgendamentoEventoTela
