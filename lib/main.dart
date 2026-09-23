329a8f3a36ff6161eaab7748f2501af237266c0c
xjuuhhh <juliasantoscursos05@gmail.com>
2026-09-23 11:22:37 -0300

    finaizado

--- lib/main.dart
+++ lib/main.dart
@@ -354,6 +354,34 @@ class _AgendamentoEventoTelaState extends State<AgendamentoEventoTela> {
             ),
             const SizedBox(height: 24),
 
+            // --- Botões de Ação Final (Cancelar e Salvar)
+            Row(
+              children: [
+                Expanded(
+                  child: OutlinedButton(
+                    onPressed: _resetarValores,
+                    style: OutlinedButton.styleFrom(
+                      foregroundColor: Colors.red,
+                      side: const BorderSide(color: Colors.red),
+                    ),
+                    child: const Text('Cancelar'),
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: _salvarformulario,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      foregroundColor: Colors.white,
+                    ),
+                    child: const Text('Salvar'),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+
           ],
         ),
       ),
